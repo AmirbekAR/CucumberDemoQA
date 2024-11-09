3a2604f35d78bdb53f3d0f296970d3d1c4d83923
amir <arynbaevamirbisnes@gmail.com>
2024-11-10 00:12:24 +0600

    some changes for pom

--- src/test/java/demo/runner/CucumberRunner.java
+++ src/test/java/demo/runner/CucumberRunner.java
@@ -10,7 +10,7 @@ import org.junit.platform.suite.api.Suite;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "demo.steps")
-@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "${CucumberTags:defaultValue}")
+@ConfigurationParameter(key = Constants.FILTER_TAGS_PROPERTY_NAME, value = "${CucumberTags:@Regression}")
 @ConfigurationParameter(key = Constants.EXECUTION_DRY_RUN_PROPERTY_NAME, value = "false")
 @ConfigurationParameter(key = "cucumber.execution.parallel.enabled", value = "false") // включения параллельного запуска
 @ConfigurationParameter(key = "cucumber.execution.parallel.config.fixed.parallelism", value = "4") // одновременное запуск браузеров
